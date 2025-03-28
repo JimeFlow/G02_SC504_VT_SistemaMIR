4c3e203e632fbd1f1a16a83fda825038ba925b72
Firomolina <163342283+Firomolina@users.noreply.github.com>
2025-03-27 21:38:06 -0600

    Add files via upload

--- SMIR/config/db.php
+++ SMIR/config/db.php
@@ -1,13 +1,1 @@
-<?php
-$usuario = '';
-$password = '';
-$host = '';
-$puerto = '';
-$sid = '';
 
-$conn = oci_connect($usuario, $password, "//$host:$puerto/$sid");
-if (!$conn) {
-    $error = oci_error();
-    die("Error de conexión: " . $error['message']);
-}
-?>
